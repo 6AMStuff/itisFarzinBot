51fbe0695ca31ce414e8014c3fe3ab2de0d4b050
Farzin <itisFarzin@gmail.com>
2025-02-02 16:26:48 +0330

    feat: rewrite plugin loader
    - Add support for loading plugins in subdirectories
    - Add return type for functions
    - Better grammar sentences

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,7 @@
 import logging
-from glob import glob
-from importlib import import_module, util
+from pathlib import Path
+from typing import Generator
+from importlib import import_module
 
 from pyrogram import Client
 from pyrogram.handlers.handler import Handler
@@ -9,31 +10,36 @@ from pyrogram.handlers.handler import Handler
 class Bot(Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.load_plugins(path="bot/builtin_plugins")
+        self.load_plugins(folder="bot/builtin_plugins")
 
-    def plugin_list(self, path: str = None):
+    def plugin_list(
+        self, folder: str = None, only_name: bool = True
+    ) -> list[str | Path]:
         return sorted([
-            name.split(".py")[0]
-            for name in glob("*.py", root_dir=path or self.plugins["root"])
+            path.stem if only_name else path
+            for path in Path(
+                (folder or self.plugins["root"]).replace(".", "/")
+            ).rglob("*.py")
         ])
 
-    def does_plugin_exist(self, plugin: str, path: str = None):
-        module_path = (
-            path and path.replace("/", ".") or self.plugins["root"]
-        ) + "." + plugin
-        return bool(util.find_spec(module_path))
-
-    def get_handlers(self, plugins: str | list[str], path: str = None):
+    def get_handlers(
+        self, plugins: str | list[str], folder: str = None
+    ) -> Generator[tuple[str, str] | tuple[Handler, int], None, None]:
         if isinstance(plugins, str):
             plugins = plugins.split(",")
 
-        for pname in plugins:
-            path = path and path.replace("/", ".") or self.plugins["root"]
-            module_path = path + "." + pname
+        _plugins = self.plugin_list(folder=folder)
 
-            if not self.does_plugin_exist(pname, path):
-                yield (pname, "Plugin not found")
+        if plugins:
+            for plugin in plugins:
+                if plugin not in _plugins:
+                    yield (plugin, "Plugin not found")
+
+        for path in self.plugin_list(folder=folder, only_name=False):
+            if path.stem not in (plugins or _plugins):
                 continue
+
+            module_path = '.'.join(path.parent.parts + (path.stem,))
             module = import_module(module_path)
 
             for name in vars(module).keys():
@@ -46,52 +52,53 @@ class Bot(Client):
                         ):
                             yield (handler, group)
 
-    def is_loaded(self, handler: Handler, group: int = 0):
+    def is_loaded(self, handler: Handler, group: int = 0) -> bool:
         if group not in self.dispatcher.groups:
             return False
         return handler in self.dispatcher.groups[group]
 
-    def load_plugins(self, plugins: str | list[str] = None, path: str = None):
+    def load_plugins(
+        self, plugins: str | list[str] = None, folder: str = None
+    ) -> dict[str, str]:
         result = {}
 
-        for handler in self.get_handlers(
-            plugins or self.plugin_list(path=path),
-            path=path
-        ):
-            if isinstance(handler[1], str):
+        for handler in self.get_handlers(plugins, folder=folder):
+            if isinstance(handler[0], str):
                 result[handler[0]] = handler[1]
                 logging.warning(handler[1])
-            elif not self.is_loaded(*handler):
-                self.add_handler(*handler)
-                result[handler[0].callback.__name__] = "Loaded"
-                logging.info(f"Loaded: {handler[0].callback.__name__}")
             else:
-                result[handler[0].callback.__name__] = "Failed to load"
-                logging.warning(
-                    f"Failed to load: {handler[0].callback.__name__}, "
-                    "already loaded"
-                )
+                callback_name = handler[0].callback.__name__
+                if not self.is_loaded(*handler):
+                    self.add_handler(*handler)
+                    result[callback_name] = "Handler loaded"
+                    logging.info(f"{callback_name} handler has been loaded")
+                else:
+                    result[callback_name] = "Failed to load handler"
+                    logging.warning(
+                        f"Failed to load {callback_name} handler, "
+                        "because it is already loaded"
+                    )
         return result
 
     def unload_plugins(
-        self, plugins: str | list[str] = None, path: str = None
+        self, plugins: str | list[str] = None, folder: str = None
     ):
         result = {}
 
-        for handler in self.get_handlers(
-            plugins or self.plugin_list(path=path)
-        ):
-            if isinstance(handler[1], str):
+        for handler in self.get_handlers(plugins, folder=folder):
+            if isinstance(handler[0], str):
                 result[handler[0]] = handler[1]
                 logging.warning(handler[1])
-            elif self.is_loaded(*handler):
-                self.remove_handler(*handler)
-                result[handler[0].callback.__name__] = "Unloaded"
-                logging.info(f"Unloaded: {handler[0].callback.__name__}")
             else:
-                result[handler[0].callback.__name__] = "Failed to unload"
-                logging.warning(
-                    f"Failed to unload: {handler[0].callback.__name__}, "
-                    "not loaded already"
-                )
+                callback_name = handler[0].callback.__name__
+                if self.is_loaded(*handler):
+                    self.remove_handler(*handler)
+                    result[callback_name] = "Handler unloaded"
+                    logging.info(f"{callback_name} handler has been unloaded")
+                else:
+                    result[callback_name] = "Failed to unload handler"
+                    logging.warning(
+                        f"Failed to unload {callback_name} handler, "
+                        "it is not loaded already."
+                    )
         return result
