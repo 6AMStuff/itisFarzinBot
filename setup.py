13ede08bba99a6e0b1981332f91aedddc05aad53
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-17 19:28:20 +0330

    feat(setup.py): Add verbose
    * By default, the script will indicate what it is doing.
    
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 import os
 import shutil
+import argparse
 import subprocess
+
+parser = argparse.ArgumentParser(
+    prog="setup.py",
+    description="Help to set up basics"
+)
+
+parser.add_argument(
+    "--verbose",
+    action=argparse.BooleanOptionalAction,
+    default=True
+)
+
+args = parser.parse_args()
 
 pip_command = [
     "pip",
@@ -12,6 +26,8 @@ pip_command = [
 ]
 
 if os.path.exists("/IS_CONTAINER"):
+    if args.verbose:
+        print("Running in a container")
     pip_command += [
         "--no-cache-dir",
         "--break-system-packages",
@@ -19,10 +35,20 @@ if os.path.exists("/IS_CONTAINER"):
         "ignore"
     ]
 else:
+    if args.verbose:
+        print("Running in a normal environment")
+        print("Setting virtual environment up")
     os.system("python -m venv venv")
     os.system("source venv/bin/activate")
+
+if args.verbose:
+    print("Installing dependencies")
+else:
+    pip_command.append("-q")
 
 subprocess.call(pip_command)
 
 if not os.path.isfile("data/.env"):
     shutil.copy("data/.env.example", "data/.env")
+    if args.verbose:
+        print("Copied .env.example to .env")
