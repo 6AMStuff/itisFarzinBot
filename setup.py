00d6d58bc496a20c8716ad2f8ca037f41e0a3d9b
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-17 19:56:58 +0330

    refactor(setup.py): move environment setup to a function and update entry point
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- setup.py
+++ setup.py
@@ -18,40 +18,46 @@ parser.add_argument("--requirement", type=str, default="requirements.txt")
 
 args = parser.parse_args()
 
-pip_command = [
-    "pip",
-    "install",
-    "--disable-pip-version-check",
-    "--upgrade",
-    "--requirement",
-    args.requirement or ""
-]
 
-if os.path.exists("/IS_CONTAINER"):
-    if args.verbose:
-        print("Running in a container")
-    pip_command += [
-        "--no-cache-dir",
-        "--break-system-packages",
-        "--root-user-action",
-        "ignore"
+def setup_environment(requirement="requirements.txt", verbose=True):
+    pip_command = [
+        "pip",
+        "install",
+        "--disable-pip-version-check",
+        "--upgrade",
+        "--requirement",
+        requirement or ""
     ]
-else:
-    if args.verbose:
-        print("Running in a normal environment")
-        print("Setting virtual environment up")
-    os.system("python -m venv venv")
-    os.system("source venv/bin/activate")
 
-if args.verbose:
-    print("Installing dependencies")
-else:
-    pip_command.append("-q")
+    if os.path.exists("/IS_CONTAINER"):
+        if verbose:
+            print("Running in a container")
+        pip_command += [
+            "--no-cache-dir",
+            "--break-system-packages",
+            "--root-user-action",
+            "ignore"
+        ]
+    else:
+        if verbose:
+            print("Running in a normal environment")
+            print("Setting virtual environment up")
+        os.system("python -m venv venv")
+        os.system("source venv/bin/activate")
 
-if args.requirement:
-    subprocess.call(pip_command)
+    if verbose:
+        print("Installing dependencies")
+    else:
+        pip_command.append("-q")
 
-if not os.path.isfile("data/.env"):
-    shutil.copy("data/.env.example", "data/.env")
-    if args.verbose:
-        print("Copied .env.example to .env")
+    if requirement:
+        subprocess.call(pip_command)
+
+
+if __name__ == "__main__":
+    setup_environment(args.requirement, args.verbose)
+
+    if not os.path.isfile("data/.env"):
+        shutil.copy("data/.env.example", "data/.env")
+        if args.verbose:
+            print("Copied .env.example to .env")
