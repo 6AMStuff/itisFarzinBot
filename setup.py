c2e32d0e271f089d0a524012f97314e634f92a50
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-17 14:46:54 +0330

    feat(setup.py): install requirements packages
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 import os
 import shutil
+import subprocess
+
+pip_command = [
+    "pip",
+    "install",
+    "--upgrade",
+    "--requirement",
+    "requirements.txt"
+]
+
+if os.path.exists("/IS_CONTAINER"):
+    pip_command += ["--no-cache-dir", "--break-system-packages"]
+else:
+    os.system("python -m venv venv")
+    os.system("source venv/bin/activate")
+
+subprocess.call(pip_command)
 
 if not os.path.isfile("data/.env"):
     shutil.copy("data/.env.example", "data/.env")
