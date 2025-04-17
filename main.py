f7915a16de7e24a6b3268f72cebfa3ce73d3a79c
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-04-17 19:57:46 +0330

    feat: install dependencies of plugins
    * But it's not a prefect solution.
    
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import os
 from bot import Bot
+from setup import setup_environment
 
 from config import Config
 
-
+plugins_folder = Config.getenv("plugins_folder", "plugins")
 app = Bot(
     "data/" + str(Config.getenv("client_name", "itisFarzin")),
     api_id=Config.getenv("api_id"),
     api_hash=Config.getenv("api_hash"),
     bot_token=Config.getenv("bot_token"),
     proxy=Config.url_parser(Config.PROXY, is_a_proxy=True),
-    plugins=dict(root=Config.getenv("plugins_folder", "plugins"))
+    plugins=dict(root=plugins_folder)
 )
 
 
 if __name__ == "__main__":
+    for root, _, files in os.walk(plugins_folder, followlinks=True):
+        for file in files:
+            if file == "requirements.txt":
+                setup_environment(f"{root}/{file}", False)
     app.run()
