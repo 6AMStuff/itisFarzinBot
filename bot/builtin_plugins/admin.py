fb7867069e4771eb930333be08e0640b38353ee1
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-05-14 15:43:02 +0330

    feat(plugins/admin): add `deladmin` command
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- bot/builtin_plugins/admin.py
+++ bot/builtin_plugins/admin.py
@@ -1,14 +1,15 @@
 import time
-from sqlalchemy import select
 from pyrogram.types import Message
 from sqlalchemy.orm import Session
 from pyrogram import Client, filters
+from sqlalchemy import delete, select
 
 from config import Config, AdminDatabase
 
 
 @Client.on_message(
-    Config.IS_ADMIN & filters.command(["addadmin"], Config.CMD_PREFIXES)
+    Config.IS_ADMIN
+    & filters.command(["addadmin", "deladmin"], Config.CMD_PREFIXES)
 )
 async def admin(_: Client, message: Message):
     action = message.command[0]
@@ -38,6 +39,33 @@ async def admin(_: Client, message: Message):
                         by_user=message.from_user.id,
                         full_name=user.full_name,
                         promote_time=time.time(),
+                    )
+                )
+                session.commit()
+                await message.reply("Done.")
+        case "deladmin":
+            if not message.reply_to_message:
+                await message.reply("Reply to a user.")
+                return
+
+            if not message.reply_to_message.from_user:
+                await message.reply("Can't remove this user from admin list.")
+                return
+
+            user = message.reply_to_message.from_user
+            with Session(Config.engine) as session:
+                data = session.execute(
+                    select(AdminDatabase).where(AdminDatabase.id == user.id)
+                ).one_or_none()
+                if not data:
+                    await message.reply(
+                        f"User {user.mention} is not an admin."
+                    )
+                    return
+
+                session.execute(
+                    delete(AdminDatabase).where(
+                        AdminDatabase.id == user.id
                     )
                 )
                 session.commit()
