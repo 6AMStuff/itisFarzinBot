b63789670bbe8eb53225e7a55b570ab22923237d
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-07-24 13:05:52 +0330

    feat(main): auto set up plugins
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import uvloop
+import shutil
 import asyncio
+import logging
 import subprocess
 from bot import Bot
+from git import Repo
 from pyrogram.methods.utilities.idle import idle
 
 from config import Config
@@ -42,7 +45,39 @@ def requirements():
             )
 
 
+def setup_plugins():
+    plugins_repo = Config.getenv(
+        "plugins_repo", "https://github.com/6AMStuff/itisFarzinBotPlugins"
+    )
+    if not plugins_repo:
+        logging.warning("Skipping setting up plugins.")
+        return
+
+    repo_name = os.path.basename(plugins_repo).replace(".git", "")
+    repo_path = os.path.join(plugins_folder, repo_name)
+    if os.path.exists(repo_path):
+        repo = Repo(repo_path)
+        if plugins_repo not in [remote.url for remote in repo.remotes]:
+            logging.warning(
+                f"{repo_name} doesn't have {plugins_repo}"
+                " as one of its remotes."
+            )
+            logging.info(f"Removing {repo_name}'s folder.")
+            shutil.rmtree(repo_path)
+            Repo.clone_from(plugins_repo, repo_path)
+            logging.info(f"Cloned {repo_name}.")
+
+        repo.remote().fetch()
+        repo.git.pull()
+        logging.info(f"Updated {repo_name}.")
+    else:
+        Repo.clone_from(plugins_repo, repo_path)
+        logging.info(f"Cloned {repo_name}.")
+
+
 if __name__ == "__main__":
+    setup_plugins()
+
     if Config.getenv("test_mode") not in {"true", "1"}:
         requirements()
 
