24ef6e5fc734405970a3d91f94b23c76a554f486
Farzin Kazemzadeh <itisFarzin@proton.me>
2026-02-19 14:31:47 +0330

    refactor(types): prepare for mypy strict
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- bot/types/message.py
+++ bot/types/message.py
@@ -1,11 +1,10 @@
 import pyrogram.types
-
-from typing import TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
     import bot
 
 
 class Message(pyrogram.types.Message):
-    def __init__(self, client: "bot.Bot", **kwargs):
+    def __init__(self, client: "bot.Bot", **kwargs: Any) -> None:
         super().__init__(client=client, **kwargs)
