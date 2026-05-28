705506f559b8e5a6024021ce4acf5fe8021a9c88
Farzin Kazemzadeh <itisFarzin@proton.me>
2026-05-28 16:22:38 +0330

    feat(types): add arguments parser to message
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- bot/types/message.py
+++ bot/types/message.py
@@ -1,3 +1,4 @@
+import shlex
 import pyrogram.types
 from typing import Any, TYPE_CHECKING
 
@@ -8,3 +9,36 @@ if TYPE_CHECKING:
 class Message(pyrogram.types.Message):
     def __init__(self, client: "bot.Bot", **kwargs: Any) -> None:
         super().__init__(client=client, **kwargs)
+
+    def parse_arguments(self) -> dict[str, Any]:
+        # Limit it to filter.command
+        if not self.command:
+            return {}
+
+        raw = self.content.split(maxsplit=1)
+        if len(raw) < 2:
+            return {}
+
+        tokens = shlex.split(raw[1])
+        arguments: dict[str, Any] = {}
+
+        i = 0
+
+        while i < len(tokens):
+            token = tokens[i]
+
+            if not token.startswith("-"):
+                i += 1
+                continue
+
+            key = token.lstrip("-")
+
+            if i + 1 >= len(tokens) or tokens[i + 1].startswith("-"):
+                arguments[key] = True
+                i += 1
+                continue
+
+            arguments[key] = tokens[i + 1]
+            i += 2
+
+        return arguments
