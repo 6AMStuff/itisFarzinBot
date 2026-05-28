0cd47fdc2c73e1025e305ff9787bf39ad5f4fdb1
Farzin Kazemzadeh <itisFarzin@proton.me>
2026-05-28 17:08:50 +0330

    fix(types): use our message type in callback query
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- bot/types/callback_query.py
+++ bot/types/callback_query.py
@@ -1,10 +1,10 @@
+import bot.types
 import pyrogram.types
-from typing import Any, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    import bot
+from typing import Any
 
 
 class CallbackQuery(pyrogram.types.CallbackQuery):
+    message: bot.types.Message
+
     def __init__(self, client: "bot.Bot", **kwargs: Any) -> None:
         super().__init__(client=client, **kwargs)
