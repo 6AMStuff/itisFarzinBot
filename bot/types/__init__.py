111e069f5aa8fa1be746198885585c279ca07443
Farzin Kazemzadeh <itisFarzin@proton.me>
2026-05-29 15:11:28 +0330

    feat(types): add pyrogram type re-exports
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- bot/types/__init__.py
+++ bot/types/__init__.py
@@ -1,4 +1,24 @@
-from .callback_query import CallbackQuery
-from .message import Message
+from typing import TYPE_CHECKING
 
-__all__ = ("CallbackQuery", "Message")
+if TYPE_CHECKING:
+    from pyrogram.types import *  # pyright: ignore # noqa: F403
+
+    from .callback_query import (  # type: ignore[assignment]
+        CallbackQuery as CallbackQuery,
+    )
+    from .message import Message as Message  # type: ignore[assignment]
+else:
+    import sys
+
+    import pyrogram.types as pg_types
+
+    from .callback_query import CallbackQuery
+    from .message import Message
+
+    _current_module = sys.modules[__name__]
+    for _name in dir(pg_types):
+        if not _name.startswith("_"):
+            setattr(_current_module, _name, getattr(pg_types, _name))
+
+    _current_module.CallbackQuery = CallbackQuery
+    _current_module.Message = Message
