3e85b7f1b6e1b91bca9081f8adb149f144623051
Farzin Kazemzadeh <itisFarzin@proton.me>
2026-06-16 09:58:11 +0330

    refactor(bot): simplify _post_init call logic
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@proton.me>

--- bot/bot.py
+++ bot/bot.py
@@ -24,11 +24,4 @@ class Bot(Core, Client, metaclass=BotMeta):  # type: ignore[misc]
         self.dispatcher = Dispatcher(self)
         DataBase.metadata.create_all(Settings.engine)
 
-        for base_class in reversed(self.__class__.__bases__[0].__bases__):
-            if base_class is self.__class__ or base_class is object:
-                continue
-
-            if hasattr(base_class, "_post_init"):
-                _post_init = base_class._post_init
-                if callable(_post_init):
-                    _post_init(self)
+        super()._post_init()
